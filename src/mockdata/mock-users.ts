299e77a92f7b67d75bdf486759e372e5602b6d55
Mateus Manica <mateus.manica@sap.com>
2019-07-14 01:19:03 -0300

    added mockeddata

---
+++ src/mockdata/mock-users.ts
@@ -0,0 +1,15 @@
+import { User } from '../entities/user';
+
+export const USERS: User[] = [
+  { id: 11, nome: 'Dr Nice', email:'drnice@gmail.com', senha:'12345678' },
+  { id: 12, nome: 'Narco', email:'narco@gmail.com', senha:'12345678' },
+  { id: 13, nome: 'Bombasto', email:'bombasto@gmail.com', senha:'12345678' },
+  { id: 14, nome: 'Celeritas', email:'celeritas@gmail.com', senha:'12345678' },
+  { id: 15, nome: 'Magneta', email:'magneta@gmail.com', senha:'12345678' },
+  { id: 16, nome: 'RubberMan', email:'rubberman@gmail.com', senha:'12345678' },
+  { id: 17, nome: 'Dynama', email:'dynama@gmail.com', senha:'12345678' },
+  { id: 18, nome: 'Dr IQ', email:'driq@gmail.com', senha:'12345678' },
+  { id: 19, nome: 'Magma', email:'magma@gmail.com', senha:'12345678' },
+  { id: 20, nome: 'Tornado', email:'tornado@gmail.com', senha:'12345678' },
+  { id: 21, nome: 'test', email:'test@test.com', senha:'12345' }
+];
