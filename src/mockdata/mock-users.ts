230e171ea1bcb780b234776981fe5debd5059d64
Mateus Manica <mateus.manica@sap.com>
2019-07-15 09:59:59 -0300

    added pic field for user entity

--- src/mockdata/mock-users.ts
+++ src/mockdata/mock-users.ts
@@ -1,16 +1,12 @@
 import { User } from '../entities/user';
 
 export const USERS: User[] = [
-  { id: 0, nome: 't', email:'t@t.com', senha:'12345' },
-  { id: 1, nome: 'Dr Nice', email:'drnice@gmail.com', senha:'12345678' },
-  { id: 2, nome: 'Narco', email:'narco@gmail.com', senha:'12345678' },
-  { id: 3, nome: 'Bombasto', email:'bombasto@gmail.com', senha:'12345678' },
-  { id: 4, nome: 'Celeritas', email:'celeritas@gmail.com', senha:'12345678' },
-  { id: 5, nome: 'Magneta', email:'magneta@gmail.com', senha:'12345678' },
-  { id: 6, nome: 'RubberMan', email:'rubberman@gmail.com', senha:'12345678' },
-  { id: 7, nome: 'Dynama', email:'dynama@gmail.com', senha:'12345678' },
-  { id: 8, nome: 'Dr IQ', email:'driq@gmail.com', senha:'12345678' },
-  { id: 9, nome: 'Magma', email:'magma@gmail.com', senha:'12345678' },
-  { id: 10, nome: 'Tornado', email:'tornado@gmail.com', senha:'12345678' },
-  { id: 11, nome: 'test', email:'test@test.com', senha:'12345' }
+  { id: 0, nome: 't', email:'t@t.com', senha:'12345', pic: 'assets/img/profile/garfield.png'},
+  { id: 1, nome: 'Dr Nice', email:'drnice@gmail.com', senha:'12345678', pic: 'assets/img/profile/homer.jpeg' },
+  { id: 2, nome: 'Narco', email:'narco@gmail.com', senha:'12345678', pic: 'assets/img/profile/megaman.jpg' },
+  { id: 3, nome: 'Bombasto', email:'bombasto@gmail.com', senha:'12345678', pic: 'assets/img/profile/shrek.png' },
+  { id: 4, nome: 'Magneta', email:'magneta@gmail.com', senha:'12345678', pic: 'assets/img/profile/marsha.jpg' },
+  { id: 5, nome: 'RubberMan', email:'rubberman@gmail.com', senha:'12345678', pic: 'assets/img/profile/nemo.png' },
+  { id: 6, nome: 'Magma', email:'magma@gmail.com', senha:'12345678', pic: 'assets/img/profile/pluto.png' },
+  { id: 7, nome: 'test', email:'test@test.com', senha:'12345', pic: 'assets/img/profile/capybara.png' }
 ];
