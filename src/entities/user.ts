0a972126b9f879f27cd67219dd61f02a3f455b66
Mateus Manica <mateus.manica@sap.com>
2019-07-15 09:59:59 -0300

    added pic field for user entity

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -3,11 +3,13 @@ export class User {
     nome: string;
     email: string;
     senha: string;
+    pic: string;
 
-    constructor(id: number, nome: string, email: string, senha: string){
+    constructor(id: number, nome: string, email: string, senha: string, pic: string){
       this.id = id;
       this.nome = nome;
       this.email = email;
       this.senha = senha;
+      this.pic = pic;
     }
   }
