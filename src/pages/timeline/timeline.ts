7eb210b6b81d277e2cf8fae73340b5ab1330b1cf
Mateus Manica <mateus.manica@sap.com>
2019-07-15 20:33:44 -0300

    changed from list to object the save method

--- src/pages/timeline/timeline.ts
+++ src/pages/timeline/timeline.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { NovoPostPage } from '../novo-post/novo-post';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { Post } from '../../entities/post';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'page-timeline',
@@ -9,7 +12,14 @@ import { NovoPostPage } from '../novo-post/novo-post';
 export class TimelinePage {
   // this tells the tabs component which Pages
   // should be each tab's root Page
-  constructor(public navCtrl: NavController) {
+  // items: Observable<{}[]>;
+  constructor(public navCtrl: NavController, private db: AngularFireDatabase) {
+    // this.items = this.db.list('/posts/3').valueChanges();
+    // this.items.subscribe(posts => {
+    //   posts.forEach( (post, index) => {
+    //     console.log(post);
+    //   });
+    // });
   }
   
   goToNovoPostPage(params){
