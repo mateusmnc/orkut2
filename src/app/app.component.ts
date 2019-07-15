969b521dc5be78b21e795767fb7659e269ff6987
Mateus Manica <mateus.manica@sap.com>
2019-07-15 11:55:31 -0300

    setup AngularFire and firebase to the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,9 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { LoginPage } from '../pages/login/login';
+import { FIREBASE_CONFIG } from './firebase.config';
+import firebase from 'firebase';
+import { AngularFireDatabase } from '@angular/fire/database';
 
 @Component({
   templateUrl: 'app.html'
@@ -11,7 +14,11 @@ export class MyApp {
   
   rootPage:any = LoginPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    platform: Platform, 
+    statusBar: StatusBar, 
+    splashScreen: SplashScreen, 
+    db: AngularFireDatabase) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
