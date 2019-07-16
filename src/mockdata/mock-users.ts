eac853829c80c07bf351c5d170b47a62326dc901
Mateus Manica <mateus.manica@sap.com>
2019-07-15 21:57:45 -0300

    fixed mockdata

--- src/mockdata/mock-users.ts
+++ src/mockdata/mock-users.ts
@@ -3,7 +3,7 @@ import { User } from '../entities/user';
 export const USERS: User[] = [
   { id: 0, nome: 't', email:'t@t.com', senha:'12345', pic: 'assets/img/profile/garfield.png'},
   { id: 1, nome: 'Dr Nice', email:'drnice@gmail.com', senha:'12345678', pic: 'assets/img/profile/homer.jpeg' },
-  { id: 2, nome: 'Narco', email:'narco@gmail.com', senha:'12345678', pic: 'assets/img/profile/megaman.jpg' },
+  { id: 2, nome: 'Narco', email:'narco@gmail.com', senha:'12345678', pic: 'assets/img/profile/megaman.jpeg' },
   { id: 3, nome: 'Bombasto', email:'bombasto@gmail.com', senha:'12345678', pic: 'assets/img/profile/shrek.png' },
   { id: 4, nome: 'Magneta', email:'magneta@gmail.com', senha:'12345678', pic: 'assets/img/profile/marsha.jpg' },
   { id: 5, nome: 'RubberMan', email:'rubberman@gmail.com', senha:'12345678', pic: 'assets/img/profile/nemo.png' },
