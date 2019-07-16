fb80e0ebc829348b841884d57849cb45bc7bfe6c
Mateus Manica <mateus.manica@sap.com>
2019-07-15 21:58:36 -0300

    display list of person to add as friend

--- src/pages/pesquisar-amigos/pesquisar-amigos.ts
+++ src/pages/pesquisar-amigos/pesquisar-amigos.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { USERS } from '../../mockdata/mock-users';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { AuthProvider } from '../../providers/auth/auth';
+import { Observable } from 'rxjs';
+import { User } from '../../entities/user';
 
 @Component({
   selector: 'page-pesquisar-amigos',
@@ -8,7 +13,24 @@ import { NavController } from 'ionic-angular';
 export class PesquisarAmigosPage {
   // this tells the tabs component which Pages
   // should be each tab's root Page
-  constructor(public navCtrl: NavController) {
+  friends : Observable<any[]>;
+  friendsToAdd : User[];
+  constructor(
+    public navCtrl: NavController, 
+    private db: AngularFireDatabase, 
+    private auth: AuthProvider) {
+    this.friends = this.db.list('friends/' + this.auth.getCurrentUser().id).valueChanges(); 
+    this.friends.subscribe(
+      friends => {
+        if(friends.length == 0){
+          this.friendsToAdd = USERS;
+        }
+        console.log(friends); console.log(friends)
+      },
+      error => {this.friendsToAdd = USERS; console.log("error");});
   }
-  
+
+  addPerson($event){
+    console.log("adicionado " + $event);
+  }
 }
