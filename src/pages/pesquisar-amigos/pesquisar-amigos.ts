cd51cb218ed34f1a893d01741239a15a0594c71f
Mateus Manica <mateus.manica@sap.com>
2019-07-16 17:44:12 -0300

    amigos and pesquisar amigos funcionando

--- src/pages/pesquisar-amigos/pesquisar-amigos.ts
+++ src/pages/pesquisar-amigos/pesquisar-amigos.ts
@@ -3,7 +3,7 @@ import { NavController } from 'ionic-angular';
 import { USERS } from '../../mockdata/mock-users';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { AuthProvider } from '../../providers/auth/auth';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { User } from '../../entities/user';
 
 @Component({
@@ -11,26 +11,34 @@ import { User } from '../../entities/user';
   templateUrl: 'pesquisar-amigos.html'
 })
 export class PesquisarAmigosPage {
-  // this tells the tabs component which Pages
-  // should be each tab's root Page
+  
   friends : Observable<any[]>;
-  friendsToAdd : User[];
+  friendsToAdd : Observable<User[]>;
+
   constructor(
     public navCtrl: NavController, 
     private db: AngularFireDatabase, 
     private auth: AuthProvider) {
+
     this.friends = this.db.list('friends/' + this.auth.getCurrentUser().id).valueChanges(); 
     this.friends.subscribe(
-      friends => {
-        if(friends.length == 0){
-          this.friendsToAdd = USERS;
-        }
-        console.log(friends); console.log(friends)
-      },
-      error => {this.friendsToAdd = USERS; console.log("error");});
+      friendsList => {
+          console.log("friends"); 
+          console.log(friendsList);
+          console.log("friends[0]" + friendsList[0]);
+          // this.friendsToAdd = USERS;
+          let idList: number[] = new Array();
+          friendsList.forEach(friend => {
+            idList.push(friend.id);
+            console.log(friend.id);
+          })
+          console.log(USERS.filter(fta => !idList.includes(fta.id)));
+          this.friendsToAdd = of(USERS.filter(fta => !idList.includes(fta.id)));
+      });
   }
 
   addPerson($event){
+    this.db.list(`friends/` + this.auth.getCurrentUser().id).push({id : $event})
     console.log("adicionado " + $event);
   }
 }
