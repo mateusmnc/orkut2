852159d373c65f18bafec23ff492e24cb4abcb3b
Mateus Manica <mateus.manica@sap.com>
2019-07-27 17:05:13 -0300

    refactored User constructor

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -5,13 +5,13 @@ export class User {
   pwd: string;
   pic?: string;
 
-  constructor(uuid: string, name: string, email: string, pwd: string, pic: string){
-    this.uuid = uuid;
+  constructor(name: string, email: string, pwd: string, pic?: string, uuid?: string){
     this.name = name;
     this.email = email;
     this.pwd = pwd;
+    this.uuid = uuid;
     this.pic = pic;
-    }
+  }
 }
 
 export interface UserResponse{
