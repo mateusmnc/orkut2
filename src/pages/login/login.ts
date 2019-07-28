cf54ea7c66401acdcd9ea1ba57a4cb28447c20f3
Mateus Manica <mateus.manica@sap.com>
2019-07-28 15:20:53 -0300

    added single login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -4,6 +4,7 @@ import { BemVindoAoOrkut2Page } from '../bem-vindo-ao-orkut2/bem-vindo-ao-orkut2
 import { TabsControllerPage } from '../tabs-controller/tabs-controller';
 import { AuthProvider } from '../../providers/auth/auth';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { User } from '../../entities/user';
 
 @Component({
   selector: 'page-login',
@@ -20,16 +21,35 @@ export class LoginPage {
 
       this.loginForm = this.formBuilder.group({
         email:['', Validators.compose([Validators.required, Validators.email])],
-        senha:['', Validators.compose([Validators.required])]
+        password:['', Validators.compose([Validators.required])]
       });
   
   }
 
-  login(){
-    if(this.auth.login(this.loginForm.value.email, this.loginForm.value.senha)){
+  ionViewDidLoad(){
+    if(this.auth.isUserSignedIn()){
       this.goToTabsControllerPage();
     }
   }
+
+  login(){
+    if(!this.loginForm.valid){
+      return;
+    }
+
+    let userToLogin = this.buildUserFromForm(this.loginForm.value);
+    if(this.auth.login(userToLogin)){
+      this.goToTabsControllerPage();
+    }
+  }
+
+  private buildUserFromForm(loginForm){
+    let user = new User();
+    user.email = loginForm.email;
+    user.pwd = loginForm.password;
+    return user;
+  }
+
   goToTabsControllerPage(){
     this.navCtrl.push(TabsControllerPage);
   }
