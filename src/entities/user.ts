b17b2577d1f1a7dd52a1f62e806c90dbe526af59
Mateus Manica <mateus.manica@sap.com>
2019-07-28 11:19:16 -0300

    remote signup in place

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,27 +1,8 @@
 export class User {
-  uuid?: string;
+  uid?: string;
   name: string;
   email: string;
-  pwd: string;
+  pwd?: string;
   pic?: string;
 
-  constructor(name: string, email: string, pwd: string, pic?: string, uuid?: string){
-    this.name = name;
-    this.email = email;
-    this.pwd = pwd;
-    this.uuid = uuid;
-    this.pic = pic;
-  }
-}
-
-export interface UserResponse{
-  result?: {
-    email?: string;
-    uuid?: string;
-  };
-
-  error?: {
-    code?: string;
-    message?: string;
-  }
 }
