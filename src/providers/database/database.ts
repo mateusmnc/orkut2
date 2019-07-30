11620625235d55028e69fc3f4ff217f08cdfce78
Mateus Manica <mateus.manica@sap.com>
2019-07-30 02:22:43 -0300

    pesquisar-amigos and amigos pages now display their lists

--- src/providers/database/database.ts
+++ src/providers/database/database.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { User } from '../../entities/user';
 import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import 'rxjs/add/operator/first';
+import { map, take } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 /*
   Generated class for the DatabaseProvider provider.
@@ -12,11 +15,13 @@ import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
 export class DatabaseProvider {
   
   private users: AngularFireList<User>;
+  private friends: AngularFireList<string>;
+  friendsSubscription: any;
 
   constructor(private db: AngularFireDatabase) {
     this.users = this.db.list<User>('users');
   }
-
+  
   public async saveNewUser(newUser: User){
     return this.users
       .push(newUser)
@@ -26,14 +31,62 @@ export class DatabaseProvider {
     });
   }
 
-  public getUserByUid(uid: string): User {
-    let user: User;
-    let usersUpdated = this.users.valueChanges().subscribe( userList => {
-      user = userList.filter(usr => usr.uid == uid)[0];
-    });
-    usersUpdated.unsubscribe();
-    console.log("userByUid: " + uid);
-    console.log(user);
-    return user;
+  async getUserByUid(uid: string) {
+    try {
+      let userList = await this.users.valueChanges().first().toPromise();
+      return userList.filter(usr => usr.uid == uid)[0];  
+    } catch (e) {
+      console.log(e);
+      return null;
+    }
+  }
+
+  public getUserByUserId(userId: string): Observable<User> {
+    return this.users.valueChanges()
+      .pipe(
+        map(us => us.filter(u => u.userId === userId)[0]),
+        take(1));
+  }
+
+  public getFriendsUserIds(user: User): Observable<string[]> {
+    this.friends = this.db.list('friends/' + user.userId);
+    return this.friends.valueChanges().pipe();
+  }
+
+  public getFriendsByUserIds(friends: string[]): Observable<User[]>{
+    return this.users.valueChanges().pipe(
+      map(users => {
+        return users.filter( usr => friends.includes(usr.userId))
+      })
+    );
+  }
+
+  public getUserToBeFriend(friends: string[]): Observable<User[]>{
+    return this.users.valueChanges().pipe(
+      map(users => {
+        console.log("response");
+        console.log(users);
+        console.log(friends);
+          return users.filter(user => {
+            if(!(user.userId in friends)){
+              return user;
+            }
+          });
+      })
+    );
+  }
+
+  public addFriend(user: User, friendUserId: string){
+    if(this.friends == null){
+      this.friends = this.db.list('friends/' + user.userId);
+    }
+    this.friends.set(friendUserId, friendUserId);
+  }
+  
+  public removeFriend(user: User, friendUserId: string){
+    if(this.friends == null){
+      this.friends = this.db.list('friends/' + user.userId);
+    }
+    this.friends.remove(friendUserId);
   }
 }
