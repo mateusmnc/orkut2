bfe964885d98c7aa8fa7c272218ae3e044b56d27
Mateus Manica <mateus.manica@sap.com>
2019-08-01 04:13:04 -0300

    timeline fully working

--- src/entities/post.ts
+++ src/entities/post.ts
@@ -1,13 +1,16 @@
+import { User } from "./user";
+
 export class Post {
-    uuid: string;
-    communicatorUserId: string;
-    communicatorUserName: string;
-    authorUserId: string;
-    authorUserName: string;
-    text: string;
-    img: string;
-    imgPath: string;
-    visibility: boolean;
-    timestamp: number;
+    uuid?: string;
+    communicatorUserId?: string;
+    communicatorUserName?: string;
+    authorUserId?: string;
+    authorUserName?: string;
+    text?: string;
+    img?: string;
+    imgPath?: string;
+    visibility?: boolean;
+    timestamp?: number;
+    user?: User;
 }
 
