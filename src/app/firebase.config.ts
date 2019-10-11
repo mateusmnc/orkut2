aa3e5df499f729fa7ce9e9cc556352ed5e826512
Mateus Manica <mateusmnc@users.noreply.github.com>
2019-10-10 23:36:41 -0300

    Update firebase.config.ts

--- src/app/firebase.config.ts
+++ src/app/firebase.config.ts
@@ -1,9 +1,9 @@
 export const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyBjsGkUqgw-YVR7HNj7puNrX6UE6jpD1_U",
-    authDomain: "project-7365915399863662840.firebaseapp.com",
-    databaseURL: "https://project-7365915399863662840.firebaseio.com",
-    projectId: "project-7365915399863662840",
-    storageBucket: "project-7365915399863662840.appspot.com",
-    messagingSenderId: "1000312078252",
-    appId: "1:1000312078252:web:0ea47cdf431739f7"
+  apiKey: "<fill with data provided by google firebase>",
+    authDomain: "<fill with data provided by google firebase>",
+    databaseURL: "<fill with data provided by google firebase>",
+    projectId: "<fill with data provided by google firebase>",
+    storageBucket: "<fill with data provided by google firebase>",
+    messagingSenderId: "<fill with data provided by google firebase>",
+    appId: "<fill with data provided by google firebase>"
   };
